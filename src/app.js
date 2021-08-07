b5c7084492afa68e209c00b12dbec2316b888c8a
João Pedro <joao.costa@fdte.io>
2021-08-07 18:08:48 -0300

    Implementing app.js

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const Koa = require("koa");
+const logger = require("koa-logger");
+const bodyParser = require("koa-bodyparser");
+const errorHandling = require("../src/middlewares/errorHandling");
+const { connectDatabase } = require("../db/index");
+require("dotenv").config();
+
+const router = require("../src/routes/routes");
+
+const app = new Koa();
+
+connectDatabase();
+
+app
+  .on("error", (err, ctx) => console.error("Server Error: ", err, ctx))
+  .use(logger())
+  .use(bodyParser())
+  .use(errorHandling)
+  .use(router.routes());
+
+module.exports = app;
